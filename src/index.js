54dbaa14a3db1b1029abe94b241467a764117416
StanislavProtashchuk <stasprotaskhyk@gmail.com>
2022-02-23 00:22:06 +0200

    styles

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
-import './sass/main.scss';
+import './style.css'
+import Notiflix from 'notiflix';
 
 const value = document.querySelector(".search-form");
+const reset = document.querySelector(".reset");
+const foo = "";
 let inputValue = "";
 
 value.addEventListener("submit", onFormSubmit);
+reset.addEventListener("click", onReset);
 
 function onFormSubmit(e) {
     e.preventDefault();
     inputValue = e.currentTarget.elements.comment;
     const reverseValue = inputValue.value.split(" ").reverse().join(" ");
-    if (inputValue !== "") {
+    if (inputValue.value === "") {
+        Notiflix.Notify.failure(`Sorry, you must enter some comment. Please try again.`);
+    }
+    if (inputValue.value !== "") {
+        Notiflix.Notify.success(`Greate! Look for result!`);
         inputValue.value = reverseValue;
     }
+}
+
+function onReset(){
+    inputValue = "";
 }
