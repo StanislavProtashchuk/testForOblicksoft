15cc9d77cac828995dbd914719bfce737242a612
StanislavProtashchuk <stasprotaskhyk@gmail.com>
2022-02-22 22:42:35 +0200

    done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 import './sass/main.scss';
+
+const value = document.querySelector(".search-form");
+let inputValue = "";
+
+value.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(e) {
+    e.preventDefault();
+    inputValue = e.currentTarget.elements.comment;
+    const reverseValue = inputValue.value.split(" ").reverse().join(" ");
+    if (inputValue !== "") {
+        inputValue.value = reverseValue;
+    }
+}
